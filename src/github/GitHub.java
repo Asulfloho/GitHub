41f28512275f7bc5dcb26ffb9f86dba8fcf0df51
Daw1 <Daw1@MEDREGAL12>
2025-02-13 15:18:41 +0000

    Versión 1 (20250205) Hola Mundo

---
+++ src/github/GitHub.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package github;
+
+/**
+ *
+ * @author Marcos Jiménez Toledo <b04m17118g@canariaseducacion.es>
+ */
+public class GitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Inicio de práctica 4.3: GitHub + Neatbens");
+    }
+    
+}
